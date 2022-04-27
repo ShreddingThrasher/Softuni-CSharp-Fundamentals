c082eacbd980d4cfc1089ac4401d0407abe21042
ShreddingThrasher <100221813+ShreddingThrasher@users.noreply.github.com>
2022-04-27 14:56:49 +0300

    Initial commit

---
+++ Associative Arrays - Lab/CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> numbers = new SortedDictionary<double, int>();
+
+            double[] input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (numbers.ContainsKey(input[i]))
+                {
+                    numbers[input[i]]++;
+                }
+                else
+                {
+                    numbers.Add(input[i], 1);
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Associative Arrays - Lab/OddOccurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string toLowerCase = input[i].ToLower();
+
+                if (words.ContainsKey(toLowerCase))
+                {
+                    words[toLowerCase]++;
+                }
+                else
+                {
+                    words.Add(toLowerCase, 1);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', words.Where(x => x.Value % 2 != 0).Select(x => x.Key)));
+        }
+    }
+}
